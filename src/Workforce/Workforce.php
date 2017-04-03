92a3c97292554704638f736a7dfc060bd8d1e645
Tony DeStefano <spokanetony@gmail.com>
2017-04-03 13:32:41 -0700

    Changed license to MIT, added composer file and added base url to Workforce object

--- src/Workforce/Workforce.php
+++ src/Workforce/Workforce.php
@@ -5,15 +5,19 @@ namespace Sentric\Workforce;
 class Workforce {
 
 	private $token;
+	private $base_url;
 
 	/**
 	 * Workforce constructor.
 	 *
 	 * @param string $token
+	 * @param string $base_url
 	 */
-	public function __construct( $token )
+	public function __construct( $token, $base_url )
 	{
-		$this->setToken( $token );
+		$this
+			->setToken( $token )
+			->setBaseUrl( $base_url );
 	}
 
 	/**
@@ -31,8 +35,44 @@ class Workforce {
 	 */
 	public function setToken( $token )
 	{
+		$token = trim( $token );
+
 		$this->token = $token;
 
 		return $this;
 	}
+
+	/**
+	 * @return mixed
+	 */
+	public function getBaseUrl()
+	{
+		return $this->base_url;
+	}
+
+	/**
+	 * @param mixed $base_url
+	 *
+	 * @return Workforce
+	 */
+	public function setBaseUrl( $base_url )
+	{
+		$base_url = trim( $base_url );
+
+		if ( substr( strtolower( $base_url ), 0, 4 ) != 'http' )
+		{
+			$base_url = 'http://' . $base_url;
+		}
+
+		if ( substr( $base_url, -1 ) != '/' )
+		{
+			$base_url .= '/';
+		}
+
+		$this->base_url = $base_url;
+
+		return $this;
+	}
+
+
 }
